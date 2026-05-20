37400cf1bcb2e70b0ff656dafbb4f9b9681e188d
Maxwell Wijaya <maxwellwijaya00@gmail.com>
2026-05-19 17:59:25 -0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
-import { useEffect } from 'react'
-import { ModalContext } from './ModalContext'
-import GetStartedModal from './components/GetStartedModal'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import HomePage from './pages/HomePage'
-import TeamPage from './pages/TeamPage'
-import ReviewsPage from './pages/ReviewsPage'
+import { useState } from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import { ModalContext } from "./ModalContext";
+import GetStartedModal from "./components/GetStartedModal";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
+import TeamPage from "./pages/TeamPage";
+import ReviewsPage from "./pages/ReviewsPage";
+
+import { Analytics } from "@vercel/analytics/next";
 
 function ScrollToTop() {
-  const { pathname } = useLocation()
-  useEffect(() => { window.scrollTo(0, 0) }, [pathname])
-  return null
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
 }
 
 export default function App() {
-  const [modalOpen, setModalOpen] = useState(false)
+  const [modalOpen, setModalOpen] = useState(false);
 
   return (
     <ModalContext.Provider value={{ openModal: () => setModalOpen(true) }}>
+      <Analytics />
       <BrowserRouter>
         <ScrollToTop />
         <Navbar />
@@ -29,8 +34,13 @@ export default function App() {
           <Route path="/reviews" element={<ReviewsPage />} />
         </Routes>
         <Footer />
-        {modalOpen && <GetStartedModal open={modalOpen} onClose={() => setModalOpen(false)} />}
+        {modalOpen && (
+          <GetStartedModal
+            open={modalOpen}
+            onClose={() => setModalOpen(false)}
+          />
+        )}
       </BrowserRouter>
     </ModalContext.Provider>
-  )
+  );
 }
